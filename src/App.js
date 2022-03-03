0bb62ac5a5aa46ceef19be32fc387a751e2ef571
Benton J <phixmeplease@gmail.com>
2022-03-03 15:06:12 -0500

    crypto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavLinks from './components/navlinks';
+import Content from './components/content';
+
+function App() {
+  return (
+    <div className="App">
+      <NavLinks />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navlinks.js
@@ -0,0 +1,18 @@
+import "./navlinks.css"
+
+function NavLinks() {
+    return (<div className="nav-links-body">
+        <div className="nav-links-name-container">
+            <svg xmlns="http://www.w3.org/2000/svg" class="nav-links-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5 11l7-7 7 7M5 19l7-7 7 7" />
+            </svg>
+            <p className="nav-links-name">CryptoForUkraine.net</p>
+        </div>
+        <div className="nav-links-links-container">
+            <a href="https://twitter.com/ukraine/status/1497594592438497282">Twitter Post</a>
+            <a href="#">President's Twitter</a>
+        </div>
+    </div>)
+}
+
+export default NavLinks;

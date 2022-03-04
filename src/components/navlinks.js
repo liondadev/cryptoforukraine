1c55527bc4a70ed213f44954c76d78d1f2970043
Benton J <phixmeplease@gmail.com>
2022-03-03 21:35:27 -0500

    remove .net from the navbar logo

--- src/components/navlinks.js
+++ src/components/navlinks.js
@@ -7,7 +7,7 @@ function NavLinks() {
             <svg xmlns="http://www.w3.org/2000/svg" class="nav-links-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M5 11l7-7 7 7M5 19l7-7 7 7" />
             </svg>
-            <p className="nav-links-name">CryptoForUkraine.net</p>
+            <p className="nav-links-name">CryptoForUkraine</p>
         </div>
         <div className="nav-links-links-container">
             <a href="https://www.coinbase.com/signup">Buy Crypto</a>
